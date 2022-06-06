99b3e46102e5d33dbad77752f3e845bc3b51a003
fomka94 <fom-aina@mail.ru>
2022-06-07 00:30:12 +0900

    Initial Commit

---
+++ 12-timer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  12-timer
+//
+//  Created by Фомина Айина Валерьевна on 07.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
